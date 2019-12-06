f9ee990e26652fff97ff3d3015b4afdf47c9b938
Joearrowsmith <joearrowsmith98@gmail.com>
2019-12-06 00:39:55 +0000

    Made simple voting work for n agents and added vscode to gitignore

--- agent_5_simple_voting.py
+++ agent_5_simple_voting.py
@@ -2,87 +2,79 @@ from pedlar.agent import Agent
 from collections import deque
 import numpy as np
 
+
 class SimpleVotingAgent(Agent):
     name = "Mean_voting_agent"
     
-    def __init__(self, agents_list, agents_params, rets_length, verbose=False, **kwargs):
+    def __init__(self, agents_list, agents_params, agents_require_pos_func,
+                 rets_length, verbose=False, **kwargs):
         super().__init__(**kwargs)
-        
+        num_agents = len(agents_list)
+        num_params = len(agents_params)
+        num_pos_func_bool = len(agents_require_pos_func)
+        assert num_agents == num_params == num_pos_func_bool, f'{num_agents}!={num_params}!={num_pos_func_bool}'
+
         self.agents_inst = []
         for a, p in zip(agents_list, agents_params):
-            combined_dict = {**kwargs,**p}
+            combined_dict = {**kwargs, **p}
             print(combined_dict)
             self.agents_inst.append(a(**combined_dict))
-        
+        self.agents_req_pos_func = agents_require_pos_func
+
         self.verbose = verbose
         self.last_mid = None
         self.rets = deque(maxlen=rets_length)
         
     def on_tick(self, bid, ask, time=None):
-        mid = (bid  + ask) / 2 
+        mid = (bid + ask) / 2 
         if self.verbose:
             print(f'Tick: {mid: .05f}, {time}')
         if self.last_mid is None:
             self.last_mid = mid
             return
-        
         ret = np.log(mid) - np.log(self.last_mid)
-        
         self.rets.append(ret)
-        
         if len(self.rets) < self.rets.maxlen:
             return
-        
-        current_std = np.std(self.rets)
-        
-        simple_macd = self.agents_inst[0]
-        dt = self.agents_inst[1]
-        
-        macd_signal = simple_macd.on_tick(bid, ask, time)
-        dt_signal = dt.on_tick(bid, ask, time)
-        
-        
-        if macd_signal is None or dt_signal is None:
-            return
-        
-        simple_macd_norm = macd_signal/current_std
-        simple_macd_posfunc = np.tanh(simple_macd_norm)
-        dt_posfunc = dt_signal
-        
-        
-        
-        voting = np.mean([simple_macd_posfunc, dt_posfunc])
-        print(voting)
+        rolling_std = np.std(self.rets)
+
+        pos_funcs = []
+        for a, req_pf in zip(self.agents_inst, self.agents_req_pos_func):
+            signal = a.on_tick(bid, ask, time)
+            if signal is None:
+                continue
+            if req_pf:
+                signal_vol_norm = signal/rolling_std
+                pos_func = np.tanh(signal_vol_norm)
+            else:
+                pos_func = signal
+            pos_funcs.append(pos_func)
+            
+        voting = np.mean(pos_funcs)
+        print(pos_funcs, voting)
         pass
 
-    
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     from agent_1_simple_macd import SimpleMACDAgent
     from agent_4_decision_tree import DecisionTreeAgent
     agents_list = [SimpleMACDAgent, DecisionTreeAgent]
-    agents_params = [{'fast_length':120,
-                      'slow_length':250,
-                     'make_order':False},
-                     {'horizon':100, 'max_depth':3,
-                      'fast_length':120, 'slow_length':250,
-                      'make_order':False}
-    ]
+    agents_params = [{'fast_length': 120,
+                      'slow_length': 250,
+                      'make_order': False},
+                     {'horizon': 100,
+                      'max_depth': 3,
+                      'fast_length': 120,
+                      'slow_length': 250,
+                      'make_order': False}]
+    agents_require_pos_func = [True, False]
     backtest = True
     if backtest:
-        agent = SimpleVotingAgent(agents_list,  agents_params, 100, backtest='data/backtest_GBPUSD_12_hours.csv')
+        agent = SimpleVotingAgent(agents_list,  agents_params, agents_require_pos_func, 100,
+                                  backtest='data/backtest_GBPUSD_12_hours.csv')
     else:
-        agent = SimpleVotingAgent(agents_list, agents_params, 100, username='joe', password='1234',
-                          ticker='tcp://icats.doc.ic.ac.uk:7000',
-                          endpoint='http://icats.doc.ic.ac.uk')
+        agent = SimpleVotingAgent(agents_list, agents_params, agents_require_pos_func, 100,
+                                  username='joe', password='1234',
+                                  ticker='tcp://icats.doc.ic.ac.uk:7000',
+                                  endpoint='http://icats.doc.ic.ac.uk')
     agent.run()
-
-    
-
-
-
-
-
-
-
-
