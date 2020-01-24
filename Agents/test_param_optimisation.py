f64820ad80629d3dbcc20171270c3c828db4363c
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-24 01:03:36 +0000

    fixed unit tests

--- Agents/test_param_optimisation.py
+++ Agents/test_param_optimisation.py
@@ -2,16 +2,16 @@
 
 from Agents.param_optimisation_random import random_search_max_expected_return
 from Agents.param_optimisation_explicit import explicit_search_max_expected_return, \
-        print_optimisation_outputs
+                                                print_optimisation_outputs
 
 
 def expicit_optimise_with_agent_1(backtest,
                                   verbose=False, sort=False):
     from Agents.agent_1_simple_macd import SimpleMACDAgent
-    test_cases = {'fast_length': [20,  60],
+    test_cases = {'fast_length': [20, 60],
                   'slow_length': [40, 120],
                   'rets_length': 'slow_length',
-                  'signal_mean': [1, 5],}
+                  'signal_mean_length': [1, 5]}
     test_param_balances = explicit_search_max_expected_return(
         SimpleMACDAgent, test_cases,
         backtest=backtest,
@@ -26,10 +26,10 @@ def expicit_optimise_with_agent_1(backtest,
 def random_optimise_with_agent_1(backtest,
                                  n=20, verbose=False, sort=True):
     from Agents.agent_1_simple_macd import SimpleMACDAgent
-    search_dict = {'fast_length': [5,   400, int],
+    search_dict = {'fast_length': [5, 400, int],
                    'slow_length': [20, 1000, int],
                    'rets_length': 'slow_length',
-                   'signal_mean': [1, 25, int]}
+                   'signal_mean_length': [1, 25, int]}
     test_param_balances = random_search_max_expected_return(
         SimpleMACDAgent, search_dict, n,
         backtest, verbose, sort
@@ -45,7 +45,7 @@ def expicit_optimise_with_agent_2(backtest,
     test_cases = {'fast_length': [120]*2,
                   'slow_length': [250]*2,
                   'rets_length': 'slow_length',
-                  'signal_mean': [1, 5],
+                  'signal_mean_length': [1, 5],
                   'stop_loss_scaling': [1.1, 1.5],
                   'take_profit_scaling': [3.5, 5.0]}
     test_param_balances = explicit_search_max_expected_return(
@@ -65,7 +65,7 @@ def random_optimise_with_agent_2(backtest,
     search_dict = {'fast_length': [5, 400, int],
                    'slow_length': [20, 1000, int],
                    'rets_length': 'slow_length',
-                   'signal_mean': [1, 25, int],
+                   'signal_mean_length': [1, 25, int],
                    'stop_loss_scaling': [1.1, 6.0, float],
                    'take_profit_scaling': [0.1, 6.0, float]}
     test_param_balances = random_search_max_expected_return(
@@ -80,10 +80,10 @@ def random_optimise_with_agent_2(backtest,
 def expicit_optimise_with_agent_3(backtest,
                                   verbose=False, sort=False):
     from Agents.agent_3_ret_bound_risk_macd import RetBoundRiskMACDAgent
-    test_cases = {'fast_length': [120]*2,
-                  'slow_length': [250]*2,
+    test_cases = {'fast_length': [120] * 2,
+                  'slow_length': [250] * 2,
                   'rets_length': 'slow_length',
-                  'signal_mean': [1, 5],
+                  'signal_mean_length': [1, 5],
                   'ret_upper_scaling_factor': [2.5, 3.0],
                   'ret_lower_scaling_factor': [2.5, 3.0]}
     test_param_balances = explicit_search_max_expected_return(
@@ -103,7 +103,7 @@ def random_optimise_with_agent_3(backtest,
     search_dict = {'fast_length': [5, 200, int],
                    'slow_length': [150, 300, int],
                    'rets_length': 'slow_length',
-                   'signal_mean': [1, 25, int],
+                   'signal_mean_length': [1, 25, int],
                    'ret_upper_scaling_factor': [0.1, 6.0, float],
                    'ret_lower_scaling_factor': [0.1, 6.0, float]}
     test_param_balances = random_search_max_expected_return(
@@ -118,10 +118,10 @@ def random_optimise_with_agent_3(backtest,
 def expicit_optimise_with_agent_4(backtest,
                                   verbose=False, sort=False):
     from Agents.agent_4_decision_tree import DecisionTreeAgent
-    test_cases = {'fast_length': [120]*2,
-                  'slow_length': [250]*2,
+    test_cases = {'fast_length': [120] * 2,
+                  'slow_length': [250] * 2,
                   'rets_length': 'slow_length',
-                  'signal_mean': [1, 5],
+                  'signal_mean_length': [1, 5],
                   'prediction_horizon': [200, 250],
                   'max_depth': [1, 3],
                   'target_profit': [0.1, 0.2]}
@@ -142,7 +142,7 @@ def random_optimise_with_agent_4(backtest,
     search_dict = {'fast_length': [5, 200, int],
                    'slow_length': [150, 300, int],
                    'rets_length': 'slow_length',
-                   'signal_mean': [1, 25, int],
+                   'signal_mean_length': [1, 25, int],
                    'prediction_horizon': [20, 500, int],
                    'max_depth': [1, 5, int],
                    'target_profit': [0.01, 0.5, float]}
