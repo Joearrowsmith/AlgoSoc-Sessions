4f1921699322fec847f76a50fa6b7f15538a4a2e
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-30 20:44:20 +0000

    adding linear agent with Expected return optimisation

--- Agents/agent_7_linear_model_custom_loss.py
+++ Agents/agent_7_linear_model_custom_loss.py
@@ -4,11 +4,23 @@ Agent that uses a linear model to make decisions.
 
 from Agents.core import Core
 from collections import deque
-from sklearn.linear_model import LinearRegression
 
+import tensorflow as tf
+
+def linear_model(num_rets, loss_func):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(1, activation="tanh",  # we are predicting a position that needs to be between 1 and -1
+                              input_shape=(num_rets,))
+    ])
+    model.compile(optimizer='rmsprop',
+                  loss=loss_func)
+    return model
+
+def loss_expected_return(predicted_pos, next_return):
+    return - tf.reduce_mean(predicted_pos * next_return)
 
 class LinearAgent(Core):
-    name = "Linear_Agent_MSE"
+    name = "Linear_Agent_Custom_Loss"
 
     def __init__(self, train_data_length, predict_ahead, **kwargs):
         super().__init__(**kwargs)
@@ -16,34 +28,39 @@ class LinearAgent(Core):
         assert predict_ahead >= 0, "Must be positive predict ahead."
         self.predict_ahead = int(predict_ahead)
         self.train_data = deque(maxlen=train_data_length)
-        self.test_data = deque(maxlen=train_data_length)
+        self.train_y_data = deque(maxlen=train_data_length)
 
         self.warmup = int(train_data_length / 2)
-        self.lin_model = LinearRegression()
-        self.regressor = None
+        self.x_input_size = self.rets_length - 1 - predict_ahead
+        self.lin_model = linear_model(self.x_input_size, loss_expected_return)
+
+        self.counter = 0
 
     def core_on_tick(self, bid, ask, time=None):
         '''Called on every tick update.'''
+        self.counter += 1
         if len(self.rets) < self.rets.maxlen:
             return
         train = list(self.rets)[:-(1 + self.predict_ahead)]
         target = sum(list(self.rets)[-(1 + self.predict_ahead):])
 
         self.train_data.append(train)
-        self.test_data.append(target)
+        self.train_y_data.append([target])
 
-        if len(self.test_data) > self.warmup:
-            self.regressor = self.lin_model.fit(list(self.train_data),
-                                                list(self.test_data))
-            pred_y = self.regressor.predict([list(self.rets)[(1 + self.predict_ahead):]])
+        if len(self.train_y_data) > self.warmup:
+            if self.counter % 100 == 0:
+                # Training period
+                self.lin_model.fit(list(self.train_data), list(self.train_y_data),
+                                   epochs=10, verbose=0)
+            pred_y = self.lin_model.predict([list(self.rets)[(1 + self.predict_ahead):]])
             self.set_signal(pred_y)
             self.order(bid, ask)
 
     def order(self, bid, ask):
-        signal = self.signal_value
-        if signal > 0:
+        pos = self.signal_value
+        if pos > 0.25:
             self.core_buy(bid, ask)
-        elif signal < 0:
+        elif pos < -0.25:
             self.core_sell(bid, ask)
 
     def core_order_open(self, est_order_open_price, order_type, bid, ask):
@@ -53,8 +70,8 @@ class LinearAgent(Core):
         pass
 
 
-def main(train_data_length=20, predict_ahead=10,
-         rets_length=30, signal_mean_length=5, make_orders=True,
+def main(train_data_length=1000, predict_ahead=20,
+         rets_length=30, signal_mean_length=3, make_orders=True,
          verbose=True, backtest=None):
     if backtest is None:
         agent = LinearAgent(train_data_length, predict_ahead,
